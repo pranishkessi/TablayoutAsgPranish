114c8df1ffb468fcf773fa685f98f1b413ce1cb3
Pranish Kessi <pranishkessi10@gmail.com>
2019-11-11 12:24:15 +0545

    Second Commit

---
+++ app/src/main/java/com/si/tablayoutasgpranish/LoginFragment.java
@@ -0,0 +1,78 @@
+package com.si.tablayoutasgpranish;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.si.tablayoutasgpranish.implement.InterfaceClassIMPL;
+import com.si.tablayoutasgpranish.implement.TabLayoutInterface;
+import com.si.tablayoutasgpranish.model.Datacenter;
+
+
+public class LoginFragment extends Fragment {
+  EditText loginName, loginPassword;
+  Button btnlogin;
+
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        loginName = view.findViewById(R.id.loginName);
+        loginPassword = view.findViewById(R.id.loginPass);
+        btnlogin = view.findViewById(R.id.btnLogin);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        return view;
+    }
+
+
+    public void DataStore(){
+        Datacenter datacenter = new Datacenter();
+        String name = "", password = "";
+        if (!TextUtils.isEmpty(loginName.getText().toString())){
+            name = loginName.getText().toString();
+
+            if (!TextUtils.isEmpty(loginPassword.getText().toString())){
+                password = loginPassword.getText().toString();
+            }
+
+            else {
+                loginName.setError("Enter Name");
+                return;
+            }
+
+            TabLayoutInterface impl = new InterfaceClassIMPL();
+            if (impl.Checkuser(name,password)){
+                Toast.makeText(getActivity(), "Welcome" + name,  Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getActivity(),)
+            }
+        }
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/si/tablayoutasgpranish/NavMain.java
@@ -0,0 +1,14 @@
+package com.si.tablayoutasgpranish;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NavMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nav_main);
+    }
+}
